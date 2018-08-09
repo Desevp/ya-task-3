f75ddb507af6c234c0d77ee89c00079a7b71a2c5
Jane Des <desevp@gmail.com>
2018-08-09 16:29:49 +0300

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Подключим зависимости
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+var config = {
+	server: {
+		baseDir: './'
+	},
+	tunnel: false,
+	host: 'localhost',
+	port: 9000
+};
+
+
+gulp.task('reload', function () {
+  return gulp.src('*.js')
+    .pipe(reload({
+			stream: true
+		}));
+});
+
+// Задача по умолчанию
+gulp.task('default', ['serve']);
+
+// Локальный сервер, слежение
+gulp.task('serve', function() {
+  browserSync.init(config);
+  gulp.watch(['*.js'],['reload']);
+
+});
