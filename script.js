f58a48d038966a01d2d5dcd5ec9c5c9e633dcab5
Jane Des <desevp@gmail.com>
2018-08-11 17:38:02 +0300

    Added check function to findMinSubArray

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ function getSchedule(data) {
   let powers = Array(24).fill(data.maxPower);
   let prices = Array(24).fill(0);
 
+  const defaultMode = {
+    'day': {
+      from: 7,
+      to: 23
+    },
+    'night': {
+      from: 23,
+      to: 7
+    }
+  }
+
   // Заполняем почасовую цену
 
   for (let key in rates) {
@@ -21,6 +32,7 @@ function getSchedule(data) {
       prices.fill(rates[key].value, 0, to);
     }
   }
+
 
   console.log(schedule);
   console.log(devices);
@@ -38,36 +50,127 @@ function getSchedule(data) {
     console.log(duration);
     console.log(mode);
 
-    console.log(Math.round(findMinAverage(prices, duration)[1]*power/1000 * 10000)/10000);
+    console.log('!!!!');
+
+
+    let curModeFunc = checkMode.bind(null, mode);
+
+
+
+
+
+
+
+
+    // console.log(dayFunc(4));
+
+    // checkMode(hour, mode)
+
+    // console.log(Math.round(findMinSubArray(prices, duration)[1]*power/1000 * 10000)/10000);
+    console.log(Math.round(findMinSubArray(prices, duration, curModeFunc)[1] * 10000)/10000);
+
+    // console.log(checkMode(23, mode));
   }
 
-  function findMinAverage(nums, k) {
-    var curr_min = 0;
-    for (var i = 0; i < k; i++) {
-      curr_min += nums[i];
+
+  function findFirstSubArray(array, l, checkFunc) {
+    let begin =0;
+    let result = -1;
+    let minimum = 0;
+
+    while ((begin <= array.length - l) && (result < 0)) {
+      result = checkLength(l);
     }
 
+    return {
+      beginIndex: ((begin + l - 1) < array.length?begin:-1),
+      min: minimum
+    }
+
+    function checkLength(k) {
+      if (k!==0) {
+        if (checkFunc(begin + k)) {
+          minimum += array[begin + k];
+          return checkLength(k-1);
+        }
+        else {
+          begin +=k;
+          minimum = 0;
+          return -1;
+        }
+      }
+      else {
+        begin++;
+        return true;
+      }
+    }
+  }
+
+  // TEST
+
+  // function test(a) {
+  //   if ((a === 1) || (a === 5)){
+  //     return false;
+  //   }
+  //
+  //   return true;
+  // }
+
+
+  function findMinSubArray(nums, k, checkFunc) {
+    let beginValue = findFirstSubArray(nums, k, checkFunc);
+    let curr_min = beginValue.min;
+
+    console.log(beginValue);
+
     var max_so_far = curr_min;
-    var begin_index = 0;
+    var begin_index = beginValue.beginIndex;
+    var falseCount = 0; // показывает сколько осталось до свободного значения
 
-    for (var j = k; j < nums.length + k - 1; j++) {
-      if (j < nums.length) {
-        curr_min += (nums[j] - nums[j - k]);
 
-      } else {
-        curr_min += (nums[j - nums.length] - nums[j - k]);
+    for (var j = k + begin_index; j < nums.length + k - 1; j++) {
+
+
+      let nextIndex = (j < nums.length)?j: j - nums.length;
+      curr_min += (nums[nextIndex] - nums[j - k]);
+
+      if (!(checkFunc(nextIndex))) {
+        falseCount = k;
       }
 
+      console.log(nextIndex + '_____');
 
-      if (curr_min < max_so_far) {
-        max_so_far = curr_min;
-        begin_index = j - k + 1;
+      if (falseCount === 0) {
+        console.log(curr_min);
+        if (curr_min < max_so_far) {
+          max_so_far = curr_min;
+          begin_index = j - k + 1;
+        }
       }
-
+      else {
+        console.log(curr_min + 'плохое');
+        falseCount--;
+      }
     }
     return [begin_index, max_so_far];
   }
 
+  function checkMode(mode, hour) {
+    switch (mode) {
+      case 'day':
+        if ((hour >= defaultMode[mode].from) && (hour < defaultMode[mode].to)) {
+          return true;
+        }
+        return false;
+      case 'night':
+        if (((hour >= defaultMode[mode].from) && (hour < 24)) || (hour < defaultMode[mode].to)) {
+          return true;
+        }
+        return false;
+      default:
+        return true;
+    }
+  }
 
   function _comparareDevices(obj1, obj2) {
     return (obj2.duration - obj1.duration)||(_weightMode(obj2.mode) - _weightMode(obj1.mode))||(obj2.power - obj1.power);
